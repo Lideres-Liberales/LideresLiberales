60331af39aa3c27331eda8cb873faf593603aff6
Rafael José <rafael_jose_calderon@hotmail.com>
2024-08-20 16:53:23 -0300

    corrigiendo archivos apps.py

--- llweb/users/apps.py
+++ llweb/users/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class PersonaConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'persona'
+    name = 'users'
+    verbose_name = 'Usuarios'
