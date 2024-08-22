439f0d8ed7e49f484d7863062d38a141a75b7df2
Rafael José <rafael_jose_calderon@hotmail.com>
2024-08-22 19:11:06 -0300

    paginacion, :(

--- llweb/press/views.py
+++ llweb/press/views.py
@@ -6,4 +6,7 @@ class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
     context_object_name = 'articles'
-    paginate_by = 10
+    paginate_by = 6
+
+    def get_queryset(self):
+        return self.model.objects.select_related('author')
